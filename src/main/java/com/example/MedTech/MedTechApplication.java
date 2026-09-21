bd23ee1914f9196143303de129ff52c6fcdb2181
Guilherme Miranda Presot <787864@sga.pucminas.br>
2026-09-21 11:03:28 -0300

    Commit inicial - spring initialize

---
+++ src/main/java/com/example/MedTech/MedTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.MedTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MedTech/MedTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MedTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
